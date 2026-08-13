659efa62352b4550a175dacd0ff6a316fcec4209
stevepizza2000 <arthurbratz1@gmail.com>
2026-08-13 16:53:51 -0300

    metade do modalEmail

--- forno-residencial-iot/src/ModalEmail.jsx
+++ forno-residencial-iot/src/ModalEmail.jsx
@@ -2,11 +2,35 @@ import { useState } from "react";
 
 function ModalEmail({ModalEmailAberto, setModalEmail}){
 
+    const [carregando, setCarregando] = useState(false);
+    const [novoEmail, setNovoEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+
     if (!ModalEmailAberto) return null;
 
     return(
-        <>
-        </>
+        <div id="sub-modal-trocar-email">
+            
+        <h2 id="Trocar E-mail">Trocar E-mail</h2>
+
+        <form action="form-trocar-email" noValidate>
+
+        <div id="campo-trocar-email">
+            <label htmlFor="change-email">Novo E-mail</label>
+            <input onChange={(e) => setEmail(e.target.value)} type="email" name="email" id="change-email" placeholder="Digite o novo E-mail" required/>
+        </div>
+
+        <div id="campo-colocar-senha">
+            <label htmlFor="password-required">Senha Atual</label>
+            <input type="password" name="password" id="password-required" placeholder="Digite sua senha" required/>
+        </div>
+
+        <button type="submit" disabled={carregando}>{carregando ? "Carregando..." : "Entrar"}</button>
+
+        </form>
+
+        </div>
     );
 
 
